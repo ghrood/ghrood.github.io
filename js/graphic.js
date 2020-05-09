1f03c0f12a02d9febbdda1d51cecefba90d9a5b8
ghrood <65083828+ghrood@users.noreply.github.com>
2020-05-09 15:44:38 -0400

    Create graphic.js

---
+++ js/graphic.js
@@ -0,0 +1,59 @@
+//Edit 'key' and 'columns' to connect your spreadsheet
+
+//enter google sheets key here
+var key =
+  "https://docs.google.com/spreadsheets/d/1l6V8uQxZr_NBgv44riJsaJuIt3g-8UhYF6gLASjBep8/edit?usp=sharing";
+
+//"data" refers to the column name with no spaces and no capitals
+//punctuation or numbers in your column name
+//"title" is the column name you want to appear in the published table
+var columns = [{
+  "data": "company",
+  "title": "Company"
+}, {
+  "data": "incentives",
+  "title": "Claims"
+}, {
+  "data": "share",
+  "title": "Total Awarded"
+}];
+
+$(document).ready(function() {
+
+  function initializeTabletopObject() {
+    Tabletop.init({
+      key: key,
+      callback: function(data, tabletop) {
+        writeTable(data); //call up datatables function
+      },
+      simpleSheet: true,
+      debug: false
+    });
+  }
+
+  initializeTabletopObject();
+
+  function writeTable(data) {
+    //select main div and put a table there
+    //use bootstrap css to customize table style: http://getbootstrap.com/css/#tables
+    $('#graphic').html(
+      '<table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-condensed table-responsive" id="mySelection"></table>'
+    );
+
+    //initialize the DataTable object and put settings in
+    $("#mySelection").DataTable({
+      "autoWidth": false,
+      "data": data,
+      "columns": columns,
+      "order": [
+        [2, "desc"]
+      ], //order on second column
+      "pagingType": "simple" //no page numbers
+        //uncomment these options to simplify your table
+        //"paging": false,
+        //"searching": false,
+        //"info": false
+    });
+  }
+});
+//end of writeTable
